d1cf25bfa16719244d7b75271979b511be4bbfc9
PotatoCat0325 <hanc02095@gmail.com>
2023-08-08 02:07:26 +0900

    Small Changes

--- Riot_Proto/Assets/Script/Ability/ThunderDrop.cs
+++ Riot_Proto/Assets/Script/Ability/ThunderDrop.cs
@@ -11,6 +11,8 @@ public class ThunderDrop : AbilityBase
 
     [SerializeField] GameObject thunder;
     Player player;
+
+    [SerializeField] float distance;
     // Start is called before the first frame update
     public override void Start()
     {
@@ -37,12 +39,30 @@ public class ThunderDrop : AbilityBase
     {
         for(int i = 0; i < level; i++)
         {
-            if (GameManager.instance.curEnemys != null && GameManager.instance.curEnemys.Count == 0) continue; 
-            Transform target = GameManager.instance.curEnemys[Random.Range(0, GameManager.instance.curEnemys.Count)].transform;
+            List<GameObject> nearbyEnemies = GetNearbyEnemies();
+            if (nearbyEnemies != null && nearbyEnemies.Count == 0) continue; 
+            Transform target = nearbyEnemies[Random.Range(0, nearbyEnemies.Count)].transform;
             if (target == null) continue; 
             Instantiate(thunder, new Vector3(target.position.x, 0, target.position.z), Quaternion.identity).GetComponent<Thunder>()
                 .SetDamage(defaultDamage * level);
             yield return new WaitForSeconds(0.3f);
         } 
+    }
+
+    private List<GameObject> GetNearbyEnemies()
+    {
+        List<GameObject> nearbyEnemies = new List<GameObject>();
+
+        GameObject player = GameManager.instance.player.gameObject;
+        foreach (GameObject enemy in GameManager.instance.curEnemys)
+        {
+            float distance = Vector3.Distance(new Vector3(0,0,0), enemy.transform.position);
+            if (distance <= this.distance)
+            {
+                nearbyEnemies.Add(enemy);
+            }
+        }
+
+        return nearbyEnemies;
     }
 }

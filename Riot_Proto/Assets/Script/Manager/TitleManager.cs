8274e8443f3be18cfb81decd9003777c28a0a7ce
jmjeon55335 <jmjeon5533@gmail.com>
2023-08-24 23:24:07 +0900

    Character Select Animation

--- Riot_Proto/Assets/Script/Manager/TitleManager.cs
+++ Riot_Proto/Assets/Script/Manager/TitleManager.cs
@@ -12,6 +12,16 @@ public class TitleManager : MonoBehaviour
     [SerializeField] Transform CharButtonParant;
     [SerializeField] Transform StageButtonParant;
     [SerializeField] Button OptionButton;
+    [Space(10)]
+    [SerializeField] RawImage CharImage;
+    [SerializeField] Transform explainPanel;
+    [Space(10)]
+    public List<Sprite> explainSprite = new();
+    Image explainImage;
+
+    [SerializeField] RawImage SelectRawImage;
+    Vector3 returnPos;
+
 
     private void Awake()
     {
@@ -19,7 +29,9 @@ public class TitleManager : MonoBehaviour
     }
     private void Start()
     {
-        OptionButton.onClick.AddListener(()=>SceneManager.instance.Option(0));
+        explainImage = explainPanel.GetChild(0).GetComponent<Image>();
+        explainPanel.gameObject.SetActive(false);
+        OptionButton.onClick.AddListener(() => SceneManager.instance.Option(0));
         CharButtonInit();
         StageButtonInit();
         InitPanel(0);
@@ -31,20 +43,55 @@ public class TitleManager : MonoBehaviour
             Panel[i].SetActive(false);
         }
         Panel[index].SetActive(true);
+        Panel[1].transform.GetChild(0).gameObject.SetActive(true);
+        explainPanel.gameObject.SetActive(false);
+        CharImage.color = Color.clear;
+        CharImage.transform.localScale = new Vector3(1,1,1);
     }
-
     public void CharButtonInit() //캐릭터 버튼 초기화
     {
         for (int i = 0; i < CharButtonParant.childCount; i++)
         {
             int j = i;
-            var button = CharButtonParant.GetChild(i).GetChild(0).GetComponent<Button>();
+            var buttonPos = CharButtonParant.GetChild(i);
+            var button = buttonPos.GetChild(0).GetComponent<Button>();
             button.onClick.AddListener(() =>
             {
+                SelectRawImage = button.transform.GetChild(0).GetComponent<RawImage>();
+                returnPos = buttonPos.transform.position;
+                Panel[1].transform.GetChild(0).gameObject.SetActive(false);
+                CharImage.texture = SelectRawImage.texture;
+                CharImage.transform.position = SelectRawImage.transform.position;
+                CharImage.color = Color.white;
+
                 SceneManager.instance.CharIndex = j;
-                InitPanel(2);
+
+                SelectMove(true);
             });
         }
+    }
+    public void SelectMove(bool OnOff)
+    {
+        StartCoroutine(SelectCharMove(OnOff));
+    }
+    IEnumerator SelectCharMove(bool OnOff)
+    {
+        if (!OnOff)
+        {
+            SelectRawImage = null;
+            explainPanel.gameObject.SetActive(false);
+            CharImage.transform.DOMove(returnPos, 1);
+            yield return CharImage.transform.DOScale(new Vector3(1, 1, 1), 1).WaitForCompletion();
+            CharImage.color = Color.clear;
+        }
+        else
+        {
+            CharImage.transform.DOLocalMove(new Vector3(-700, 100), 1);
+            yield return CharImage.transform.DOScale(new Vector3(1, 1, 1) * 1.5f, 1).WaitForCompletion();
+            CharImage.color = Color.white;
+        }
+        explainPanel.gameObject.SetActive(OnOff);
+        Panel[1].transform.GetChild(0).gameObject.SetActive(!OnOff);
     }
     public void StageButtonInit()
     {

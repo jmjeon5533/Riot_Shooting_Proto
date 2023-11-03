23564b3a14d0077218d76f70223d9b99ea4fbccb
CloudAscend <bkm4763kk@gmail.com>
2023-11-03 17:20:43 +0900

    wave 9 update

--- Riot_Proto/Assets/Script/Enemy/Mage7.cs
+++ Riot_Proto/Assets/Script/Enemy/Mage7.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class Mage7 : EnemyBase
 {
     [SerializeField] Animator anim;
-    [SerializeField] float cosHeight;
-    private int monsterCount;
+    [SerializeField] float cosHeight = 5;
+    [SerializeField] float cosWeight = 0.5f;
+    [SerializeField] int monsterCnt;
+    [SerializeField] float spawnCooltime = 0.08f;
     protected override void Awake()
     {
         base.Awake();
@@ -21,6 +23,19 @@ public class Mage7 : EnemyBase
         isAttack = true;
         anim.SetTrigger("Attack");
         yield return new WaitForSeconds(0.5f);
+        for (int i = 0; i < monsterCnt; i++)
+        {
+            //float height = Mathf.Sin(i * 2) * cosHeight;
+            float height = Mathf.Sin(i * cosWeight) * cosHeight;
+            for (int j = 0; j < 2; j++)
+            {
+                var enemy = PoolManager.Instance.GetObject("Bat3", new Vector3(13, height, 0)).GetComponent<Bat3>();
+                enemy.movedir = Vector2.left;
+                enemy.MovePos = new Vector3(-13, height, 0);
+                height *= -1;
+            }
+            yield return new WaitForSeconds(spawnCooltime);
+        }
 
         isAttack = false;
     }
@@ -30,15 +45,6 @@ public class Mage7 : EnemyBase
     }
     protected override void Update()
     {
-        
-        if (isAttack) {
-            monsterCount += 1;
-            var enemy = PoolManager.Instance.GetObject("Bat3", new Vector3(13, Mathf.Sin(monsterCount) * cosHeight, 0)).GetComponent<Bat3>();
-            enemy.movedir = Vector2.left;
-            enemy.MovePos = new Vector3(-13, Mathf.Sin(Time.time) * cosHeight, 0);
-        }
-
-
         base.Update();
     }
     protected override void Dead()

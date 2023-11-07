e1212123b15225699f8e977a1be6ec2a675866ed
jmjeon5533 <jmjeon5533@gmail.com>
2023-11-07 20:31:36 +0900

    Active Image Change

--- Riot_Proto/Assets/Script/Enemy/Mage6.cs
+++ Riot_Proto/Assets/Script/Enemy/Mage6.cs
@@ -25,35 +25,37 @@ public class Mage6 : EnemyBase
 
     private void OnEnable()
     {
-        if(!isSpawned)
+        if (!isSpawned)
         {
             isSpawned = true;
             return;
-        }    
+        }
         var shield = PoolManager.Instance.GetObject("Shield", ShieldPoint);
-            shield.transform.position = ShieldPoint.position + (Vector3.up * 3f);
-            shield = PoolManager.Instance.GetObject("Shield", ShieldPoint);
-            shield.transform.position = ShieldPoint.position + ((Vector3.down * 3f) + (Vector3.left * 1.5f));
-            shield = PoolManager.Instance.GetObject("Shield", ShieldPoint);
-            shield.transform.position = ShieldPoint.position + ((Vector3.right * 3f) + (Vector3.right * 1.5f));
+        shield.transform.position = ShieldPoint.position + (Vector3.up * 3f);
+        shield = PoolManager.Instance.GetObject("Shield", ShieldPoint);
+        shield.transform.position = ShieldPoint.position + ((Vector3.down * 3f) + (Vector3.left * 1.5f));
+        shield = PoolManager.Instance.GetObject("Shield", ShieldPoint);
+        shield.transform.position = ShieldPoint.position + ((Vector3.right * 3f) + (Vector3.right * 1.5f));
+
+        batSpawn();
     }
     IEnumerator BatSpawn()
     {
         int count = 0;
-        while(gameObject.activeSelf)
+        while (gameObject.activeSelf)
         {
             count++;
-            for(int i = 0; i < 5; i++)
+            for (int i = 0; i < 5; i++)
             {
-            var rand = count % 2 == 0 ? -1 : 1;
-            var rand2 = count % 2 == 0 ? -1 : 1;
-            var Y = Random.Range(3f,3.6f) * rand;
-            var enemy = PoolManager.Instance.GetObject("Bat3",new Vector3(13,Y + (Random.Range(0.5f,4f) * rand2),0)).GetComponent<Bat3>();
-            enemy.movedir = (GameManager.instance.player.transform.position + 
-                (Vector3)Random.insideUnitCircle * 2 - enemy.transform.position).normalized;
-            enemy.XPRate = 0;
-            enemy.ItemAddCount = 0.1f;
-            yield return new WaitForSeconds(0.01f);
+                var rand = count % 2 == 0 ? -1 : 1;
+                var rand2 = count % 2 == 0 ? -1 : 1;
+                var Y = Random.Range(3f, 3.6f) * rand;
+                var enemy = PoolManager.Instance.GetObject("Bat3", new Vector3(13, Y + (Random.Range(0.5f, 4f) * rand2), 0)).GetComponent<Bat3>();
+                enemy.movedir = (GameManager.instance.player.transform.position +
+                    (Vector3)Random.insideUnitCircle * 2 - enemy.transform.position).normalized;
+                enemy.XPRate = 0;
+                enemy.ItemAddCount = 0.1f;
+                yield return new WaitForSeconds(0.01f);
             }
             yield return new WaitForSeconds(2);
         }
@@ -84,25 +86,24 @@ public class Mage6 : EnemyBase
     {
         HP = baseHp;
         StatMultiplier();
-        batSpawn();
     }
     protected override void Update()
     {
         base.Update();
-        ShieldPoint.Rotate(new Vector3(0,0,rotSpeed * Time.deltaTime)); 
+        ShieldPoint.Rotate(new Vector3(0, 0, rotSpeed * Time.deltaTime));
     }
 
     public override void Damage(int damage, bool isCrit, string hitTag = null)
     {
-        if(ShieldPoint.childCount>0)
+        if (ShieldPoint.childCount > 0)
         {
             damage = 0;
         }
-        base.Damage(damage,isCrit,hitTag);
+        base.Damage(damage, isCrit, hitTag);
     }
     protected override void Dead()
     {
         base.Dead();
-        anim.SetBool("Death",IsDeath());
+        anim.SetBool("Death", IsDeath());
     }
 }

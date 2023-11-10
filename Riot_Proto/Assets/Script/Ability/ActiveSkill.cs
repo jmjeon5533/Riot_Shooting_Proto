6fe481d5e72b56e4ac873b97d4db602e170e2423
jmjeon5533 <jmjeon5533@gmail.com>
2023-11-10 20:18:35 +0900

    Revert "Merge remote-tracking branch 'origin/main' into jaemin"
    This reverts commit 692eadddb938a0656b940d371bd00b86989ef09a, reversing
    changes made to 48ee56e54b2647ac7919ab123583ff0b2a3096f0.

--- Riot_Proto/Assets/Script/Ability/ActiveSkill.cs
+++ Riot_Proto/Assets/Script/Ability/ActiveSkill.cs
@@ -3,18 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
-using DG.Tweening;
 
 public class ActiveSkill : MonoBehaviour, IPointerDownHandler
 {
     float maxCooltime;
     float cooltime = 0;
 
-    Color color;
-
     bool useSkill = false;
-
-    bool isOne = false;
 
     [SerializeField] Image coolTimeUI;
 
@@ -24,7 +19,7 @@ public class ActiveSkill : MonoBehaviour, IPointerDownHandler
     // Start is called before the first frame update
     void Start()
     {
-        color = coolTimeUI.color;
+        
     }
 
     private void Update()
@@ -37,11 +32,6 @@ public class ActiveSkill : MonoBehaviour, IPointerDownHandler
         //{
         //    coolTimeUI.fillAmount = 0;
         //}
-        if(!ability.IsSkillCool() && !isOne)
-        {
-            isOne = true;
-            coolTimeUI.DOColor(Color.white, 0.2f).OnComplete(() => { coolTimeUI.DOFade(0, 0.4f); });
-        }
         if(Input.GetKeyDown(KeyCode.X))
         {
             OnClick();
@@ -59,14 +49,12 @@ public class ActiveSkill : MonoBehaviour, IPointerDownHandler
         if (skill == null) return;
         if (!skill.IsSkillCool())
         {
-            coolTimeUI.color = color;
             coolTimeUI.fillAmount = 1;
             skill.Ability();
             if(g.curEnemys.Count > 0 )
                 Instantiate(g.Bomb,g.player.transform.position,Quaternion.identity);
             maxCooltime = skill.GetMaxCool();
             useSkill = true;
-            isOne = false;
         }
 
     }

d1f08553ea53d3409d40034166094645f000a376
PotatoCat0325 <hanc02095@gmail.com>
2023-11-12 01:37:26 +0900

    Bug Fix

--- Riot_Proto/Assets/Script/Bullet/PlayerBullet.cs
+++ Riot_Proto/Assets/Script/Bullet/PlayerBullet.cs
@@ -15,7 +15,8 @@ public class PlayerBullet : BulletBase
                 float chance = Random.Range(0, 100f);
                 h.GetComponent<EnemyBase>().Damage((chance <= CritRate)
                         ? (int)(Damage * CritDamage) : Damage, (chance <= CritRate) ? true : false);
-                if (h != null && h.GetComponent<EnemyBase>() != null)
+                if (h != null && h.GetComponent<EnemyBase>() != null && h.GetComponent<Alert>() == null)
+                    Debug.Log(h.gameObject);    
                     EventManager.Instance.PostNotification(Event_Type.PlayerAttacked, this, h.GetComponent<EnemyBase>());
                 PoolManager.Instance.PoolObject(BulletTag,gameObject);
                 break;
